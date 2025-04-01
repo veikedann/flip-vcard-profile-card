dc59834e6c363a6b4f45919e49d83b78aeee0cd1
Machi <37982193+veikedann@users.noreply.github.com>
2025-04-01 19:53:50 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+export const config = {
+    cardWidth: '300px',
+    cardHeight: '400px',
+    frontColor: 'linear-gradient(45deg, #3a1c71, #d76d77)',
+    backColor: 'linear-gradient(45deg, #24243e, #302b63)',
+    flipDuration: '0.8s',
+    profileImage: 'images/avatar.svg',
+    name: 'Your Name',
+    title: 'Software Developer',
+    bio: 'Passionate developer with 5 years of experience in web technologies.',
+    details: {
+        email: 'yourname@yourname.com',
+        location: 'New York, USA',
+        skills: ['JavaScript', 'React', 'Node.js'],
+        socialLinks: {
+            behance: 'https://www.behance.net/',
+            twitter: 'https://twitter.com/',
+            github: 'https://github.com/'
+        }
+    }
+};
+
